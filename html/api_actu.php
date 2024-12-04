44d54866927b94d5702bdbc32316d8418f924af7
Egoris <egoris@guardiaschool.fr>
2024-12-04 19:23:14 +0100

    api mode

--- html/api_actu.php
+++ html/api_actu.php
@@ -54,7 +54,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/style.css" id="theme-link">
+        <link rel="stylesheet" href="../css/style.css" id="theme-link1">
         <link rel="stylesheet" href="../css/api.css">
         <title>Page De Actualiter</title>
     </head>
