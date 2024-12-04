0b49629ed2e057f8274d89c0f6f8cd528d70f6f4
Egoris <egoris@guardiaschool.fr>
2024-12-04 10:44:17 +0100

    Update api_actu.php

--- html/api_actu.php
+++ html/api_actu.php
@@ -64,7 +64,7 @@
     <header>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="/css/style.css">
         <link rel="stylesheet" href="css/api.css">
         <title>Page De actu cyber<title>
     </header>
