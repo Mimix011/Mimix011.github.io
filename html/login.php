9d5ac26675e9d824d6608fea3e475ed8db6b65e1
Ilyass <iboinahery@guardiaschool.fr>
2024-12-04 12:06:01 +0100

    login_template pour jaja
    *Echange des taches*

--- html/login.php
+++ html/login.php
@@ -41,6 +41,8 @@ if(isset($_POST['envoi'])){
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login</title>
+	<link rel="stylesheet" href="../css/Login_style.css">
+	lin
 </head>
 <body>
 	<form method="POST" action="">
