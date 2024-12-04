3e5db1eaba35cbeac6d1f338cdd99e644a85fbec
Egoris <egoris@guardiaschool.fr>
2024-12-04 10:51:09 +0100

    s

--- html/admin.php
+++ html/admin.php
@@ -6,37 +6,39 @@ require_once(__DIR__ . '/functions.php');
 
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Site de recettes - Page d'accueil</title>
-	<link
-        	href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        	rel="stylesheet"
-	>
-</head>
-<body class="d-flex flex-column min-vh-100">
-	<div class="container">
-    	<!-- inclusion de l'entête du site -->
-    	<?php require_once(__DIR__ . '/header.php'); ?>
-    	<h1>Site de recettes</h1>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Site de recettes - Page d'accueil</title>
+		<link
+				href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<video autoplay muted loop id="background-video">
+		<source class="video_sombre" src="../img/background_index.mp4" type="video/mp4" id="video_d">
+		Votre navigateur ne supporte pas les vidéos HTML5.
+	</video>
+	<body class="d-flex flex-column min-vh-100">
+		<div class="container">
+			<!-- inclusion de l'entête du site -->
+			<?php require_once(__DIR__ . '/header.php'); ?>
+			<h1>Site de recettes</h1>
 
-    	<!-- Formulaire de connexion -->
-    	<?php require_once(__DIR__ . '/login.php'); ?>
+			<!-- Formulaire de connexion -->
+			<?php require_once(__DIR__ . '/login.php'); ?>
 
-    	<?php if (isset($loggedUser)) : ?>
-        	<?php foreach (getRecipes($recipes) as $recipe) : ?>
-            	<article>
-                	<h3><?php echo $recipe['title']; ?></h3>
-                	<div><?php echo $recipe['recipe']; ?></div>
-                	<i><?php echo displayAuthor($recipe['author'], $users); ?></i>
-            	</article>
-        	<?php endforeach ?>
-    	<?php endif; ?>
-	</div>
+			<?php if (isset($loggedUser)) : ?>
+				<?php foreach (getRecipes($recipes) as $recipe) : ?>
+					<article>
+						<h3><?php echo $recipe['title']; ?></h3>
+						<div><?php echo $recipe['recipe']; ?></div>
+						<i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+					</article>
+				<?php endforeach ?>
+			<?php endif; ?>
+		</div>
 
-	<!-- inclusion du bas de page du site -->
-	<?php require_once(__DIR__ . '/footer.php'); ?>
-</body>
+		<!-- inclusion du bas de page du site -->
+		<?php require_once(__DIR__ . '/footer.php'); ?>
+	</body>
 </html>
